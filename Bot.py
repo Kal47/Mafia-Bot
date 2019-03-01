dc63628868537b7486ec080ceeb5ae6c5849c1cb
Eric <eandersen208@gmail.com>
2019-03-01 08:18:48 -0700

    Added Jailer Functinality

--- Bot.py
+++ Bot.py
@@ -11,13 +11,28 @@ except:
 	print('Error: Token File Not Found!')
 	raise SystemExit(0)
 	
+print('Opening Jail and Jailcell files')
+try:	
+	jail = read(open("jail.txt", "r"))		
+	jailcell = read(open("jailcell.txt", "r"))
+except:
+	print("Error: Opening jail.txt or jailcell.txt")
+	raise SystemExit(0)
+
+
+	
 print('Success!')
 print('Waiting for Ready Event')
 
 class MyClient(discord.Client):
+	jail = read(open("jail.txt", "r"))		
+	jailcell = read(open("jailcell.txt", "r"))
+	
+
 	async def on_ready(self):
 		print('Logged on as {0}!'.format(self.user))
 		print('------------------------------------------------------')
+		
 
 	async def on_message_delete(self, message):
 		print('Message Deleted!')
@@ -25,8 +40,7 @@ class MyClient(discord.Client):
 			await message.channel.send(message.author.mention + ' deleted a message! Its was \n"' + message.content + '"')
 		except:
 			print('Error Sending Deleted Message!')
-
-
+			
 	
 	async def on_message_edit(self, before, after):
 		print('Message Edited!')
@@ -35,7 +49,27 @@ class MyClient(discord.Client):
 				await before.channel.send(before.author.mention + ' Edited a message! It was \n"' + before.content + '"')
 			except:
 				print('Error Sending Edit Message!')
+				
 	
-
+	async def on_message(self, message):
+		if message.author == client.user:
+			return
+		if message.content.startswith('.help'):
+			await message.channel.send('''Mafia Bot!
+			.jailer - channel where jailer talks to the jailed
+			.jail   - channel where jailed are kept.
+			''')
+		if message.content.startswith('.jail'):
+			f = open("jail.txt", "a")
+			f.write(message.channel)
+			jail = message.channel
+		if message.content.startswith('.jailcell'):
+			f = open("jailcell.txt", "a")
+			f.write(message.channel)
+			jailcell = message.channel
+		if message.channel == jail:
+			await jailcell.send(message.content)
+			
+			
 client = MyClient()
 client.run(token)
