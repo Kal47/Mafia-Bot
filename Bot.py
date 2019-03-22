033439da6992be6cfb3e7de2071ba759efa8cc93
Eric <eandersen208@gmail.com>
2019-03-22 09:05:18 -0600

    Added a fun ester egg if the bot is accused

--- Bot.py
+++ Bot.py
@@ -32,6 +32,7 @@ class MyClient(discord.Client):
 		
 		if message.author == client.user:
 			return		
+			
 		try:
 			await message.channel.send(message.author.mention + ' deleted a message! Its was \n"' + message.content + '"')
 		except:
@@ -51,11 +52,28 @@ class MyClient(discord.Client):
 		#if message was sent by bot ignore
 		if message.author == client.user:
 			return	
-			
+		
+		#fun little easter egg		
+		if message.content.find("<@" + str(self.user.id) + ">") != -1:				
+			if message.content.lower().find("i accuse") != -1:
+				responces = ["no u", 
+				"Imposable! You have no evidence!", 
+				"You wouldn't accuse me unless you are part of the mafia!", 
+				"What! I never did anything!", 
+				"Don't believe them! I'm innocent!", 
+				"Where did this come from! I'm just an innocent townie", 
+				"You may have found one, but you will never find us all!", 
+				"What no! I did nothing!",
+				"I'm not mafia! I'm just a bot!",
+				"Oh OK, well I accuse " + message.author.name,
+				]
+				await message.channel.send(responces[message.id % 10])
+				print(str(message.id % 10))
+		
 		#Help command
 		if message.content == '!help':
 			await message.channel.send(
-			'''!help: Opens Help text			
+'''!help: Opens Help text			
 			
 		!purge: Deletes 500 of the most recent messages in a channel
 			-Requires User to have Administrator Privileges 
