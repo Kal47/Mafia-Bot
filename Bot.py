efe21c0b67b20017d67dc501a8b4b5eb099820db
Eric <eandersen208@gmail.com>
2019-04-10 15:04:34 -0600

    Made del and edit message only on some channels updated to discord.py 1.0.0

--- Bot.py
+++ Bot.py
@@ -15,9 +15,6 @@ print('Waiting for Ready Event')
 
 class MyClient(discord.Client):
 
-	purgeInProgress = False
-	history = []
-	
 	async def on_ready(self):
 		print('Logged on as {0}!'.format(self.user))
 		print('------------------------------------------------------')
@@ -27,30 +24,28 @@ class MyClient(discord.Client):
 	
 	async def on_message_delete(self, message):	
 		print('Message Deleted!')
-		#checks if message was in purge and ignores it	
-		for x in self.history:
-			if x.id == message.id:
-				print('Deleted Massage was in purge')		
-				return
 		
+		#ignore if it is the bots message, most likely the admin cleaning up 
 		if message.author == client.user:
 			return		
 		
 		#ree on delete
 		if message.author.permissions_in(message.channel).administrator != True:
-			try:
-				await message.channel.send(message.author.mention + ' deleted a message! Its was \n"' + message.content + '"')
-			except:
-				print('Error Sending Deleted Message!')
+			if message.channel.name == 'town-square' or message.channel.name == 'dead-chat': #only on these two chats
+				try:
+					await message.channel.send(message.author.mention + ' deleted a message! Its was \n"' + message.content + '"')
+				except:
+					print('Error Sending Deleted Message in ' + message.channel.name + '!')
 	
 	
 	async def on_message_edit(self, before, after):
 		print('Message Edited!')
 		if before.content != after.content:
-			try:
-				await before.channel.send(before.author.mention + ' Edited a message! It was \n"' + before.content + '"')
-			except:
-				print('Error Sending Edit Message!')
+			if before.channel.name == 'town-square' or before.channel.name == 'dead-chat': #only on these two chats
+				try:
+					await before.channel.send(before.author.mention + ' Edited a message! It was \n"' + before.content + '"')
+				except:
+					print('Error Sending Edit Message!')
 	
 	
 	async def on_message(self, message):
@@ -69,7 +64,7 @@ class MyClient(discord.Client):
 				"*Actually,* I'm Sheriff, and you're evil! ", 
 				"I claim Jester", 
 				"Noted",
-				"Someone in deadchat is reeing so hard right now",
+				"Someone in dead chat is reeing so hard right now",
 				"Oh OK, well I accuse " + message.author.name,
 				"I have not been paying attention at all this game...",
 				"Wait I already made a deal with town!",
@@ -80,7 +75,7 @@ class MyClient(discord.Client):
 				"I have a will.. but it doesn't fit in one message, so give me a second..",
 				"Sooner or later we all knew this day was coming",
 				"Hold my beer",
-				"I've been roleblocked literally every single night, what do you want from me?",
+				"I've been role blocked literally every single night, what do you want from me?",
 				"*muffled screaming*",
 				"*notices ur accusation* uwu what's this?"
 				]
@@ -94,31 +89,35 @@ class MyClient(discord.Client):
 !purge: Deletes the most recent messages in a channel
 	-Requires User to have Administrator Privileges 
 	-Bot Needs to have manage message privileges
-	-ONLY USE ON ONE CHANNEL AT A TIME
+
+Notes about channels. The bot will only activate on channels named "town-square" or "dead-chat"
+Make sure the names are exactly that or it will not work.
 			''')
 		
 		#Purge command. Has to be exactly that phrase to reduce the change of accidentally activating	
 		if message.content == ('!purge'):
 			print("Purge command sent!")
-			#checks if user who sent the command has permissions to manage messages which includes deleting messages
+			#checks if user who sent the command is an administrator
 			if message.author.permissions_in(message.channel).administrator == True:
 				#grab history of channel and turn it into a list using .flatten()
-				self.history = await message.channel.history(limit=300).flatten()				
+				history = await message.channel.history(limit=300).flatten()	
+				await message.channel.delete_messages(history[0: 99])
 				try:
-					await message.channel.delete_messages(self.history[0: 99])
+					#await message.channel.delete_messages(history[0: 99])
 					print("0-99")
-					await message.channel.delete_messages(self.history[100: 199])
+					await message.channel.delete_messages(history[100: 199])
 					print("100-199")
-					await message.channel.delete_messages(self.history[200: 300])	
+					await message.channel.delete_messages(history[200: 300])	
 					print("200-300")
-				except:
-					#bot does not have permissions to execute this command, enables delete message, informs the server it has an issue					
-					await message.channel.send('Error: Deleting Messages')					
+				except Exception as ex:
+					#bot does not have permissions to execute this command, informs the server it has an issue	
+					print(ex)
+					await message.channel.send('Error: Could not delete messages. Could be a permissions issue or messages are over 14 days old.')					
 					return
 				#turns re-enables delete messages when purge is done				
 			else:
 				#yell at whoever tried to run it without permissions
-				await message.channel.send('Error: You do not have permissions to use this command')
+				await message.channel.send('Error: You do not have permissions to use this command!')
 					
 client = MyClient()
 client.run(token)
---
+++ updateDiscordRewrite.bat
@@ -0,0 +1,1 @@
+pip install -U git+https://github.com/Rapptz/discord.py@rewrite#egg=discord.py[voice]
